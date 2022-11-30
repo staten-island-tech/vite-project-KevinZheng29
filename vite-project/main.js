ff5b4d032abd07823dcdd76df799f59f1c35ab82
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2022-11-30 14:00:18 -0500

    1

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,23 +1,36 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const DOM = {
+  btn: document.getElementById("btn"),
+  form: document.getElementById("form"),
+};
+
+const menu = [
+  {
+    Name: "food",
+    Calories: "idk",
+    Image:
+      "https://th.bing.com/th/id/R.c75de75b64245522b8261e73c6b3d5cb?rik=%2foQhF6Ew84NyhQ&riu=http%3a%2f%2fwww.clydefitchreport.com%2fwp-content%2fuploads%2f2012%2f08%2fchick-fil-a_meal1.jpg&ehk=D%2f%2b1OTWFrnF0hZYi6PXamS6g2WqLFqpsCh%2fkHJElb8c%3d&risl=&pid=ImgRaw&r=0",
+  },
+  {
+    Name: "food2",
+    Calories: "idkagain",
+    Image:
+      "https://external-preview.redd.it/P-BXoNiHUzwokF3B5w_WiRm5bMgiHpXtQ90-ySZmayI.jpg?auto=webp&s=51012b40212b2824e0e4e16fdb2f39a75707d360",
+  },
+];
+
+DOM.btn.addEventListener("click", function () {
+  menu.forEach((element) => {
+    DOM.form.insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="flex">
+    <img src="${element.Image}"><img>
+     <h1>${element.Name}</h1>
+     <h2>${element.Calories}</h2>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+    `
+    );
+  });
+});
