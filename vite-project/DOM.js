fdabb9edfd9e2c05f85c9ceaa2028960878138a9
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2022-12-02 13:59:19 -0500

    1

---
+++ vite-project/DOM.js
@@ -0,0 +1,5 @@
+const DOM = [
+  {
+    Type: "Appetizer",
+  },
+];
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,21 +1,3 @@
 import "./style.css";
 import { menu } from "./array";
 
-const DOM = {
-  btn: document.getElementById("btn"),
-  form: document.getElementById("form"),
-};
-
-DOM.btn.addEventListener("click", function () {
-  menu.forEach((element) => {
-    DOM.form.innerHTML = `
-      <section class="Parent">
-    <div class="Child">
-    <img src="${element.Image}" class="IMG"><img>
-     <h1>${element.Name}</h1>
-     <h2>${element.Calories}</h2>
-    </div>
-    </section>
-    `;
-  });
-});
