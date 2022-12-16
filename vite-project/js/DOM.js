28070a83d993a0f8170be40eeb7ea86e85421000
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2022-12-16 14:01:08 -0500

    1

--- vite-project/js/DOM.js
+++ vite-project/js/DOM.js
@@ -3,6 +3,8 @@ const DOM = {
   krbtn: document.getElementById("KRBTN"),
   othersbtn: document.getElementById("DrinksAndDessertsBTN"),
   MenuPage: document.getElementById("MenuPage"),
+  ChangeTheme: document.getElementById("ThemeBTN"),
+  MenuMainBody: document.getElementById("MainBody"),
 };
 
 export { DOM };
